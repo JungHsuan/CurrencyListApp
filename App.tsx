2dadc73d60c957cce26855473e1f26ca165792c0
jhlin <imjustaup@gmail.com>
2025-07-12 01:29:47 +0800

    chore: adjust for iOS

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import { StatusBar, StyleSheet, View } from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import { StatusBar, StyleSheet, SafeAreaView, View } from 'react-native';
 import CurrencyListFragment from './src/components/CurrencyListFragment';
 import Button from './src/components/Button';
 import useCurrencyData from './src/hooks/useCurrencyData';
@@ -13,25 +14,32 @@ function App() {
     setDataWithPurchasable,
   } = useCurrencyData();
   return (
-    <View style={{ ...styles.container, marginTop: StatusBar.currentHeight }}>
-      <StatusBar backgroundColor={'transparent'} barStyle={'dark-content'} />
-      <Button content="Clear Data" onPress={clearData} />
-      <Button content="Insert Data" onPress={insertData} />
-      <Button content="Show Crypto List" onPress={setDataWithCryptoList} />
-      <Button content="Show Fiat List" onPress={setDataWithFiatList} />
-      <Button
-        content="Show Purchasable List"
-        onPress={setDataWithPurchasable}
-      />
-      <CurrencyListFragment data={listData} />
-    </View>
+    <SafeAreaView style={{ ...styles.container }}>
+      <View
+        style={{
+          flex: 1,
+          paddingHorizontal: 16,
+          marginTop: StatusBar.currentHeight,
+        }}
+      >
+        <StatusBar backgroundColor={'transparent'} barStyle={'dark-content'} />
+        <Button content="Clear Data" onPress={clearData} />
+        <Button content="Insert Data" onPress={insertData} />
+        <Button content="Show Crypto List" onPress={setDataWithCryptoList} />
+        <Button content="Show Fiat List" onPress={setDataWithFiatList} />
+        <Button
+          content="Show Purchasable List"
+          onPress={setDataWithPurchasable}
+        />
+        <CurrencyListFragment data={listData} />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 16,
   },
 });
 
